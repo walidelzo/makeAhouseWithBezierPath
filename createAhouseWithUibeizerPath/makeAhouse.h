0f1f83f3ef44be3aabaa82f6748e82ef897d45b0
walid <walid@walids-MacBook-Pro.local>
2018-11-18 11:44:41 +0200

    first commit

---
+++ createAhouseWithUibeizerPath/makeAhouse.h
@@ -0,0 +1,17 @@
+//
+//  makeAhouse.h
+//  createAhouseWithUibeizerPath
+//
+//  Created by walid on 11/18/18.
+//  Copyright © 2018 walid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface makeAhouse : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ createAhouseWithUibeizerPath/makeAhouse.m
@@ -0,0 +1,21 @@
+//
+//  makeAhouse.m
+//  createAhouseWithUibeizerPath
+//
+//  Created by walid on 11/18/18.
+//  Copyright © 2018 walid. All rights reserved.
+//
+
+#import "makeAhouse.h"
+
+@implementation makeAhouse
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
